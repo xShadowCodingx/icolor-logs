b8553602d14d8c0072540761f582b4909f6867bc
Chase Quinn <xchasequinnx@gmail.com>
2024-09-26 14:35:05 -0400

    Fixed colors

--- bin/themes.js
+++ bin/themes.js
@@ -3,22 +3,22 @@ const colors = require('./colors.js')
 const themes = {
     default: {
         error(text){
-            return console.log(colors.foregrounds.red, text, colors.reset)
+            return console.log(colors.foregrounds.red + text + colors.foregrounds.reset)
         },
         success(text){
-            return console.log(colors.foregrounds.green, text, colors.reset)
+            return console.log(colors.foregrounds.green + text + colors.foregrounds.reset)
         },
         info(text){
-            return console.log(colors.foregrounds.cyan, text, colors.reset)
+            return console.log(colors.foregrounds.cyan + text + colors.foregrounds.reset)
         },
         warning(text){
-            return console.log(colors.foregrounds.yellow, text, colors.reset)
+            return console.log(colors.foregrounds.yellow + text + colors.foregrounds.reset)
         },
         log(text){
-            return console.log(colors.foregrounds.blue, text, colors.reset)
+            return console.log(colors.foregrounds.blue + text + colors.foregrounds.reset)
         },
         attention(text){
-            return console.log(colors.foregrounds.magenta, text, colors.reset)
+            return console.log(colors.foregrounds.magenta + text + colors.foregrounds.reset)
         }
     }
 }
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("HELLO")
+const themes = require('./bin/themes')
+const colors = require('./bin/colors')
+
+themes.default.error("This is an error message")
+themes.default.success("This is a success message")
+themes.default.info("This is an info message")
+themes.default.warning("This is a warning message")
+themes.default.attention("This is an attention message")
+themes.default.log("This is a log message")
